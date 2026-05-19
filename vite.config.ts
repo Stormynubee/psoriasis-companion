0bb73935b740de987e0e99654c0c598c12730694
storm <storm@example.com>
2026-05-19 13:19:26 +0530

    chore: initial project scaffolding

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Psoriasis Companion',
+        short_name: 'PsoComp',
+        theme_color: '#121212',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+  // @ts-ignore
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+});
